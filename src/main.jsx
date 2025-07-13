c279213c8a29ac4f958eedca4ee42c316275e5ed
Shreya280306 <shreyakhattar06@gmail.com>
2025-07-13 23:52:36 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 // Global server URL for API calls
 
 // IMP NOTE: After any change in main.jsx, "npm run dev" has to happen again
-export const ServerURL = "http://localhost:8000";
-// export const ServerURL = "https://askpdfvideo-dvfjcqd0hzbvd4f4.canadacentral-01.azurewebsites.net";
+// export const ServerURL = "http://localhost:8000";
+export const ServerURL = "https://askpdfvideo-dvfjcqd0hzbvd4f4.canadacentral-01.azurewebsites.net";
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
