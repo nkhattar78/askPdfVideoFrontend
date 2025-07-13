79a936abbd139151cc0c36294e0aa9c1d40c53a5
Naveen Khattar <nkhattar@microsoft.com>
2025-07-13 22:41:48 +0530

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ import react from '@vitejs/plugin-react'
 
 // Replace 'your-repo-name' with your actual GitHub repo name
 export default defineConfig({
-  base: '/RAGPdfModel/',
+  base: '/',
   plugins: [react()],
 })
 
